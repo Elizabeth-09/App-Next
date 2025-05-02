e3ea64ac2197e7d9926e57e6788e059fbc4e0a07
Elizabeth-09 <2002.k.elizabeth@gmail.com>
2025-05-02 17:20:40 -0600

    dos

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth'
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
@@ -7,7 +9,7 @@ import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
- 
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -23,6 +25,25 @@ const FormSchema = z.object({
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
  
 export type State = {
   errors?: {
@@ -32,6 +53,7 @@ export type State = {
   };
   message?: string | null;
 };
+
 
 export async function createInvoice(prevState: State, formData: FormData) {
   // Validate form using Zod
@@ -112,4 +134,5 @@ export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
+
 
